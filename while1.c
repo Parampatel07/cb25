e71275b201a49ec0d6d74e8311e6e04f43063078
Parampatel07 <90458806+Parampatel07@users.noreply.github.com>
2024-02-02 20:49:05 +0530

    while1

---
+++ while1.c
@@ -0,0 +1,14 @@
+// Write a programe to print happy birthday 100 times 
+#include<stdio.h>
+void main()
+{
+    int count = 0 ;
+    
+    while( count < 100 )
+    {
+        printf("Happy birthday \n");
+        count = count + 1 ;
+    }
+    
+    printf("\nGoodbyee...");
+}
